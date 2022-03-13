6915ec2136fc548e36877a35967652a3205de08b
Tomas Vancoillie <vancoillietomas@gmail.com>
2022-03-13 15:54:37 +0100

    Test payment logic part 1

--- cypress/integration/payment_spec.js
+++ cypress/integration/payment_spec.js
@@ -23,14 +23,31 @@ describe("payment", () => {
     cy.findByText(/devon becker/i).click();
 
     // add amount and note and click pay button
-    cy.findByPlaceholder(/amount/i).type("5");
+    const paymentAmount = "5.00";
+    cy.findByPlaceholder(/amount/i).type(paymentAmount);
     const note = uuidv4();
     cy.findByPlaceholder(/add a note/i).type(note);
+    cy.findByRole("button", { name: /pay/i }).click();
 
     // return to transactions
-    // got to personal payments
+    cy.findByRole("button", { name: /return to transactions/i }).click();
+
+    // go to personal payments
+    cy.findByRole("tab", { name: /mine/i }).click();
+
     // click on payment
+    cy.findByText(note).click({ force: true });
+
     // verify if payment was made
+    cy.findByText(`-$${paymentAmount}`).should("be.visible");
+    cy.findByText(note).should("be.visible");
+
     // verify if payment amount was deducted
+    cy.get("[data-test=sidenav-user-balance]").then((balance) => {
+      const convertedOldBalance = parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const convertedNewBalance = parseFloat(balance.text().replace(/\$|,/g));
+
+      expect(convertedOldBalance - convertedNewBalance).to.equal(parseFloat(paymentAmount));
+    });
   });
 });
